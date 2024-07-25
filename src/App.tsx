c879ff2f96fa9198a8cad645a34169136e09db8a
ConnerAiken <conner@fittedtech.com>
2024-07-25 11:49:05 -0700

    Flesh out basic css layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,45 @@
-import './App.scss';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import "./App.scss";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-function App() { 
+function App() {
   return (
     <Container id="app-container">
       <Row id="app-header">
-        <Col xs={8} id="app-title" style={{backgroundColor: 'grey'}}>
+        <Col xs={7} id="app-title">
           <h1>Weather Report 2000</h1>
         </Col>
-        <Col xs={4} id="app-actions" style={{backgroundColor: 'white'}}>
+        <Col xs={{ span: 4, offset: 1 }} id="app-actions">
           <h1>Add City Section</h1>
         </Col>
       </Row>
-      <Row id="app-content" style={{backgroundColor: 'yellow'}}> 
+      <Row id="app-content">
         <Col xs={12}>
           {[1, 2, 3, 4, 5].map((i) => (
-              <Row className="city"> 
-                <Col xs={4} className="city-sidebar" style={{backgroundColor: 'green'}}>
-                  <Row>
-                    <Col xs={12} className="city-information">
-                      <p>City {i}</p>
-                    </Col>
-                    <Col xs={12} className="city-current-weather">
-                      <p>Current Weather</p>
-                    </Col>
-                  </Row>
-                </Col>
-                <Col xs={8} className="city-historical-data" style={{backgroundColor: 'blue'}}>
-                  <h1>Historical Data</h1>
-                </Col>
+            <Row className="city">
+              <Col xs={4} className="city-sidebar">
+                <Row className="city-sidebar-row">
+                  <Col xs={12} className="city-information">
+                    <p>City {i}</p>
+                  </Col>
+                </Row>
+                <Row className="city-sidebar-row">
+                  <Col xs={12} className="city-current-weather">
+                    <p>Current Weather</p>
+                    <p>Current Weather</p>
+                    <p>Current Weather</p>
+                  </Col>
+                </Row>
+              </Col>
+              <Col xs={8} className="city-historical-data">
+                <h1>Historical Data</h1>
+              </Col>
             </Row>
           ))}
-        </Col> 
+        </Col>
       </Row>
-    </Container> 
+    </Container>
   );
 }
 
